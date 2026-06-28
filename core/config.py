1deddffc7e44a52dfe33cd0534e7cf69b2832321
omralhaj036-collab <omralhaj036@gmail.com>
2026-06-28 08:23:28 +0300

    config.py

--- core/config.py
+++ core/config.py
@@ -1,1 +1,14 @@
+PROJECT_NAME = "SADAM VOX"
 
+VERSION = "1.0.0"
+
+DEFAULT_ENGINE = "voice_cleaner"
+
+SUPPORTED_ENGINES = [
+    "voice_cleaner",
+    "voice_clone",
+    "voice_conversion",
+    "ai_singer",
+    "music_composer",
+    "mastering"
+]
