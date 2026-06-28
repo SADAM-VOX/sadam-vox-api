1c0bd23a1e799c802ad632dcd25e96656f2c3322
omralhaj036-collab <omralhaj036@gmail.com>
2026-06-28 08:18:12 +0300

    task_router.py

--- core/task_router.py
+++ core/task_router.py
@@ -1,1 +1,11 @@
+class TaskRouter:
+    """
+    يستقبل طلبات الواجهة
+    ويوجهها إلى المحرك المناسب.
+    """
 
+    def __init__(self, engine_manager):
+        self.engine_manager = engine_manager
+
+    def route(self, engine_name):
+        return self.engine_manager.get(engine_name)
