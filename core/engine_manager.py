c7747217f84450cf8b26607fb46cd5f902dbfa36
omralhaj036-collab <omralhaj036@gmail.com>
2026-06-28 08:00:36 +0300

    engine_manager.py

--- core/engine_manager.py
+++ core/engine_manager.py
@@ -1,1 +1,20 @@
+class EngineManager:
+    """
+    مسؤول عن تسجيل محركات الذكاء الاصطناعي
+    وإدارتها وتشغيلها.
+    """
 
+    def __init__(self):
+        self.engines = {}
+
+    def register(self, name, engine):
+        self.engines[name] = engine
+
+    def get(self, name):
+        return self.engines.get(name)
+
+    def exists(self, name):
+        return name in self.engines
+
+    def list_engines(self):
+        return list(self.engines.keys())
